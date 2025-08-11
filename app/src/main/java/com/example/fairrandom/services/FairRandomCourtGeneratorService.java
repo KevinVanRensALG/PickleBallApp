f6a2597c4c15b3b4c2eb5e0db9d92d9a854cf21a
kvanr <vanr0039@algonquinlive.com>
2025-08-11 15:59:28 -0400

    Fixed Nullpointer error in FairRandomCourtGeneratorService.java

--- app/src/main/java/com/example/fairrandom/services/FairRandomCourtGeneratorService.java
+++ app/src/main/java/com/example/fairrandom/services/FairRandomCourtGeneratorService.java
@@ -18,25 +18,25 @@ public class FairRandomCourtGeneratorService implements CourtPlayerGeneratorServ
         // int variable
         playerSelected = 0;
         leastGamesPlayedNumber = 0;
-        // get number of least played games
-        while(!playerMap.containsKey(leastGamesPlayedNumber)) {
-            leastGamesPlayedNumber++;
-        }
-            //  while less then 4 players selected
+        //  while less then 4 players selected
         while (playerSelected<4) {
-            //check if we have at least 4 players to choose from
-            if(playerMap.get(leastGamesPlayedNumber).size()>=(4-playerSelected)){
-                //if yes then randomly select 4 players and add then to Array
-                randomSelect(playerMap);
-            } else {
-                // if not add all players to Array and find all players with next least games played
-                while (!(playerMap.get(leastGamesPlayedNumber).isEmpty())){
-                    playerArray[playerSelected] = playerMap.get(leastGamesPlayedNumber).get(0);
-                    playerMap.get(leastGamesPlayedNumber).remove(0);
-                    playerSelected++;
-                }
+            if (!playerMap.containsKey(leastGamesPlayedNumber)){
                 leastGamesPlayedNumber++;
+            } else {
+                //check if we have at least 4 players to choose from
+                if(playerMap.get(leastGamesPlayedNumber).size()>=(4-playerSelected)){
+                    //if yes then randomly select 4 players and add then to Array
+                    randomSelect(playerMap);
+                } else {
+                    // if not add all players to Array and find all players with next least games played
+                    while (!(playerMap.get(leastGamesPlayedNumber).isEmpty())){
+                        playerArray[playerSelected] = playerMap.get(leastGamesPlayedNumber).get(0);
+                        playerMap.get(leastGamesPlayedNumber).remove(0);
+                        playerSelected++;
+                    }
+                    leastGamesPlayedNumber++;
                 }
+            }
             }
         return playerArray;
     }
